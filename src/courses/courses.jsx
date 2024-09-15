304ae4a84401b1c434e92f27a42d2c5699568c33
Mohamed Shaheen <mode999777@gmail.com>
2024-09-15 17:29:28 +0300

    add courses from fake json file

--- src/courses/courses.jsx
+++ src/courses/courses.jsx
@@ -1,254 +1,78 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-import "./courses.css"
+import BeatLoader from 'react-spinners/BeatLoader';
+
+import './courses.css';
 
 const Courses = () => {
+  const [courses, setCourses] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [delayCompleted, setDelayCompleted] = useState(false);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      try {
+        const response = await fetch('/assets/data/courses.json');
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        setTimeout(() => {
+          setCourses(data || []);
+          setLoading(false);
+          setDelayCompleted(true);
+        }, 3000);
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
+        setDelayCompleted(true);
+      }
+    };
+
+    fetchCourses();
+  }, []);
+
+  if (loading && !delayCompleted) {
+    return (
+      <div className="spinner-container">
+        <BeatLoader color="blue" loading={loading} size={20} />
+      </div>
+    );
+  }
+
   return (
     <>
-        <div className="courses gap-20">
-          <div className="box rad-6 bg-white p-relative">
-            <img className="team" src="assets/imgs/team-01.png" alt="" />
-            <img className="w-full" src="assets/imgs/course-01.jpg" alt="" />
-            <div className="content p-20">
-              <h4 className="m-0">Mastering Web Design</h4>
-              <p className="c-gray fs-14">
-                Master The Art Of Web Designing And Mocking, Prototyping And
-                Creating Web Design Architecture.
-              </p>
+      <div className="courses gap-20">
+        {error && <p>Error: {error}</p>}
+        {courses.length > 0 ? (
+          courses.map(course => (
+            <div key={course.id} className="box rad-6 bg-white p-relative">
+              <img className="team" src={course.instructorImage} alt="Instructor" />
+              <img className="w-full" src={course.thumbnail} alt="Course Thumbnail" />
+              <div className="content p-20">
+                <h4 className="m-0">{course.title}</h4>
+                <p className="c-gray fs-14">{course.description}</p>
+              </div>
+              <div className="info p-relative between-flex p-15">
+                <button className="btn-shape fs-13 bg-blue c-white b-none">
+                  Course Info
+                </button>
+                <span className="c-gray fs-13">
+                  <i className="fa-regular fa-user"></i> {course.enrollment}
+                </span>
+                <span className="c-gray fs-13">
+                  <i className="fa-solid fa-dollar-sign"></i> {course.price}
+                </span>
+              </div>
             </div>
-            <div className="info p-relative between-flex p-15">
-              <button className="btn-shape fs-13 bg-blue c-white b-none">
-                Course-Info
-              </button>
-              <span className="c-gray fs-13">
-                <i className="fa-regular fa-user"></i>
-                950
-              </span>
-              <span className="c-gray fs-13">
-                <i className="fa-solid fa-dollar-sign"></i>
-                165
-              </span>
-            </div>
-          </div>
-          <div className="box rad-6 bg-white p-relative">
-            <img className="team" src="assets/imgs/team-02.png" alt="" />
-            <img className="w-full" src="assets/imgs/course-02.jpg" alt="" />
-            <div className="content p-20">
-              <h4 className="m-0">Data Structure And Algorithms</h4>
-              <p className="c-gray fs-14">
-                Master The Art Of Data Strcuture And Famous Algorithms Like
-                Sorting, Dividing And Conquering.
-              </p>
-            </div>
-            <div className="info p-relative between-flex p-15">
-              <button className="btn-shape fs-13 bg-blue c-white b-none">
-                Course-Info
-              </button>
-              <span className="c-gray fs-13">
-                <i className="fa-regular fa-user"></i>
-                1150
-              </span>
-              <span className="c-gray fs-13">
-                <i className="fa-solid fa-dollar-sign"></i>
-                210
-              </span>
-            </div>
-          </div>
-          <div className="box rad-6 bg-white p-relative">
-            <img className="team" src="assets/imgs/team-03.png" alt="" />
-            <img className="w-full" src="assets/imgs/course-03.jpg" alt="" />
-            <div className="content p-20">
-              <h4 className="m-0">Responsive Web Design</h4>
-              <p className="c-gray fs-14">
-                Mastering Responsive Web Design And Media Queries And Know
-                Everything About Breakpoints.
-              </p>
-            </div>
-            <div className="info p-relative between-flex p-15">
-              <button className="btn-shape fs-13 bg-blue c-white b-none">
-                Course-Info
-              </button>
-              <span className="c-gray fs-13">
-                <i className="fa-regular fa-user"></i>
-                650
-              </span>
-              <span className="c-gray fs-13">
-                <i className="fa-solid fa-dollar-sign"></i>
-                90
-              </span>
-            </div>
-          </div>
-          <div className="box rad-6 bg-white p-relative">
-            <img className="team" src="assets/imgs/team-04.png" alt="" />
-            <img className="w-full" src="assets/imgs/course-04.jpg" alt="" />
-            <div className="content p-20">
-              <h4 className="m-0">Mastering Python</h4>
-              <p className="c-gray fs-14">
-                Mastering Python To Prepare For Data Science And AI And
-                Automating Things in Your Life.
-              </p>
-            </div>
-            <div className="info p-relative between-flex p-15">
-              <button className="btn-shape fs-13 bg-blue c-white b-none">
-                Course-Info
-              </button>
-              <span className="c-gray fs-13">
-                <i className="fa-regular fa-user"></i>
-                950
-              </span>
-              <span className="c-gray fs-13">
-                <i className="fa-solid fa-dollar-sign"></i>
-                250
-              </span>
-            </div>
-          </div>
-          <div className="box rad-6 bg-white p-relative">
-            <img className="team" src="assets/imgs/team-05.png" alt="" />
-            <img className="w-full" src="assets/imgs/course-05.jpg" alt="" />
-            <div className="content p-20">
-              <h4 className="m-0">PHP Examples</h4>
-              <p className="c-gray fs-14">
-                PHP Tutorials And Examples And Practice On Web Application And
-                Connecting With Databases.
-              </p>
-            </div>
-            <div className="info p-relative between-flex p-15">
-              <button className="btn-shape fs-13 bg-blue c-white b-none">
-                Course-Info
-              </button>
-              <span className="c-gray fs-13">
-                <i className="fa-regular fa-user"></i>
-                850
-              </span>
-              <span className="c-gray fs-13">
-                <i className="fa-solid fa-dollar-sign"></i>
-                150
-              </span>
-            </div>
-          </div>
-          <div className="box rad-6 bg-white p-relative">
-            <img className="team" src="assets/imgs/team-02.png" alt="" />
-            <img className="w-full" src="assets/imgs/course-02.jpg" alt="" />
-            <div className="content p-20">
-              <h4 className="m-0">Data Structure And Algorithms</h4>
-              <p className="c-gray fs-14">
-                Master The Art Of Data Strcuture And Famous Algorithms Like
-                Sorting, Dividing And Conquering.
-              </p>
-            </div>
-            <div className="info p-relative between-flex p-15">
-              <button className="btn-shape fs-13 bg-blue c-white b-none">
-                Course-Info
-              </button>
-              <span className="c-gray fs-13">
-                <i className="fa-regular fa-user"></i>
-                1150
-              </span>
-              <span className="c-gray fs-13">
-                <i className="fa-solid fa-dollar-sign"></i>
-                210
-              </span>
-            </div>
-          </div>
-          <div className="box rad-6 bg-white p-relative">
-            <img className="team" src="assets/imgs/team-03.png" alt="" />
-            <img className="w-full" src="assets/imgs/course-03.jpg" alt="" />
-            <div className="content p-20">
-              <h4 className="m-0">Responsive Web Design</h4>
-              <p className="c-gray fs-14">
-                Mastering Responsive Web Design And Media Queries And Know
-                Everything About Breakpoints.
-              </p>
-            </div>
-            <div className="info p-relative between-flex p-15">
-              <button className="btn-shape fs-13 bg-blue c-white b-none">
-                Course-Info
-              </button>
-              <span className="c-gray fs-13">
-                <i className="fa-regular fa-user"></i>
-                650
-              </span>
-              <span className="c-gray fs-13">
-                <i className="fa-solid fa-dollar-sign"></i>
-                90
-              </span>
-            </div>
-          </div>
-          <div className="box rad-6 bg-white p-relative">
-            <img className="team" src="assets/imgs/team-01.png" alt="" />
-            <img className="w-full" src="assets/imgs/course-01.jpg" alt="" />
-            <div className="content p-20">
-              <h4 className="m-0">Mastering Web Design</h4>
-              <p className="c-gray fs-14">
-                Master The Art Of Web Designing And Mocking, Prototyping And
-                Creating Web Design Architecture.
-              </p>
-            </div>
-            <div className="info p-relative between-flex p-15">
-              <button className="btn-shape fs-13 bg-blue c-white b-none">
-                Course-Info
-              </button>
-              <span className="c-gray fs-13">
-                <i className="fa-regular fa-user"></i>
-                950
-              </span>
-              <span className="c-gray fs-13">
-                <i className="fa-solid fa-dollar-sign"></i>
-                165
-              </span>
-            </div>
-          </div>
-          <div className="box rad-6 bg-white p-relative">
-            <img className="team" src="assets/imgs/team-05.png" alt="" />
-            <img className="w-full" src="assets/imgs/course-05.jpg" alt="" />
-            <div className="content p-20">
-              <h4 className="m-0">PHP Examples</h4>
-              <p className="c-gray fs-14">
-                PHP Tutorials And Examples And Practice On Web Application And
-                Connecting With Databases.
-              </p>
-            </div>
-            <div className="info p-relative between-flex p-15">
-              <button className="btn-shape fs-13 bg-blue c-white b-none">
-                Course-Info
-              </button>
-              <span className="c-gray fs-13">
-                <i className="fa-regular fa-user"></i>
-                850
-              </span>
-              <span className="c-gray fs-13">
-                <i className="fa-solid fa-dollar-sign"></i>
-                150
-              </span>
-            </div>
-          </div>
-          <div className="box rad-6 bg-white p-relative">
-            <img className="team" src="assets/imgs/team-04.png" alt="" />
-            <img className="w-full" src="assets/imgs/course-04.jpg" alt="" />
-            <div className="content p-20">
-              <h4 className="m-0">Mastering Python</h4>
-              <p className="c-gray fs-14">
-                Mastering Python To Prepare For Data Science And AI And
-                Automating Things in Your Life.
-              </p>
-            </div>
-            <div className="info p-relative between-flex p-15">
-              <button className="btn-shape fs-13 bg-blue c-white b-none">
-                Course-Info
-              </button>
-              <span className="c-gray fs-13">
-                <i className="fa-regular fa-user"></i>
-                950
-              </span>
-              <span className="c-gray fs-13">
-                <i className="fa-solid fa-dollar-sign"></i>
-                250
-              </span>
-            </div>
-          </div>
-        </div>
+          ))
+        ) : (
+          <p>No courses available</p>
+        )}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Courses
+export default Courses;
